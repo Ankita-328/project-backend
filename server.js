e8ec80d67c53cd59e44e6e1589b127281d920298
Ankita-328 <1974.ankita@gmail.com>
2025-10-16 13:44:11 +0530

    again modification

--- server.js
+++ server.js
@@ -4,29 +4,49 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const app = express();
-const authRoutes = require('./routes/authRoutes.js'); 
-const sessionRoutes = require('./routes/sessionRoutes.js'); 
-const questionRoutes = require('./routes/questionRoutes.js'); 
-const {protect} = require("./middlewares/authMiddleware");
-const { generateInterviewQuestions, generateConceptExplanation} = require("./controllers/aiController");
+const authRoutes = require('./routes/authRoutes.js');
+const sessionRoutes = require('./routes/sessionRoutes.js');
+const questionRoutes = require('./routes/questionRoutes.js');
+const { protect } = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
 
 // Middleware to handle CORS
-app.use( 
+app.use(
     cors({
         origin: "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
+let isConnected = false;
 
-connectDB();
-    
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        isConnected = true;
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.error("Error connecting to MongoDB", err);
+        process.exit(1);
+    }
+};
+
+app.use((req, res, next) => {
+    if (!isConnected) {
+        connectDB();
+    }
+    next();
+})
+
 
 // Middleware
 app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
-app.use('/api/sessions', sessionRoutes); 
+app.use('/api/sessions', sessionRoutes);
 app.use('/api/questions', questionRoutes);
 app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
 app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
@@ -36,4 +56,4 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 // const PORT = process.env.PORT || 5000
 // app.listen(PORT, ()=>console.log(`Server running at Port ${PORT}`))
 
-module.exports=app
+module.exports = app
